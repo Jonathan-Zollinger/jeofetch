937bc5abc5ff0738fa3012ff12a16b0b07dec1c7
jonathan zollinger <jonathan.zollinger@outlook.com>
2023-08-21 11:57:20 -0600

    feat!: introduce windows stats support

--- src/com/github/jonathan_zollinger/jeofetch/Main.java
+++ src/com/github/jonathan_zollinger/jeofetch/Main.java
@@ -1,10 +1,10 @@
 package com.github.jonathan_zollinger.jeofetch;
 
-import com.sun.management.OperatingSystemMXBean;
 import org.apache.commons.lang3.SystemUtils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.lang.management.ManagementFactory;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,21 +16,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        OperatingSystemMXBean osBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
-        Map<String, String> properties = new HashMap<>(Map.of(
-                "Hostname", SystemUtils.getHostName(),
-                "OS Name", osBean.getName(),
-                "OS Version", osBean.getVersion()
-        ));
+        Map<String, String[]> properties = new HashMap<>();
         if (SystemUtils.IS_OS_LINUX) {
             properties.putAll(getLinuxDistroInfo());
+        } else if (SystemUtils.IS_OS_WINDOWS) {
+            properties.putAll(getWindowsInfo("systeminfo"));
         }
         for (String property : properties.keySet()) {
-            System.out.printf("%30s = %-20s%n", property, properties.get(property));
+            System.out.printf("%30s = %-20s%n", property, String.join("\n\t", properties.get(property)));
         }
     }
 
-    static Map<String, String> getLinuxDistroInfo() throws IOException {
+    static Map<String, String[]> getLinuxDistroInfo() throws IOException {
         Path osRelease = Paths.get("/etc/os-release");
         if (!Files.exists(osRelease)) {
             throw new RuntimeException("Bad argument." + osRelease + "does not exist.");
@@ -43,13 +40,51 @@ public class Main {
             throw new RuntimeException("cannot read " + osRelease);
         }
         Scanner osScanner = new Scanner(osRelease);
-        Map<String, String> properties = new HashMap<>();
+        Map<String, String[]> properties = new HashMap<>();
         while (osScanner.hasNextLine()) {
             String[] lines = Arrays.stream(osScanner.nextLine().split("="))
                     .map(s -> s.replace("\"", ""))
                     .toArray(String[]::new);
-            properties.put(lines[0], lines[1]);
+            properties.put(lines[0], new String[]{lines[1]});
         }
         return properties;
+    }
+
+    static Map<String, String[]> getWindowsInfo(String... command) {
+        Map<String, String[]> windowsProperties = new HashMap<>();
+        for (String thisCommand : command) {
+
+            Process process;
+            try {
+                process = Runtime.getRuntime().exec("cmd /c " + thisCommand);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            String lastKey = "";
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    if (line.isEmpty()) {
+                        continue;
+                    }
+                    System.out.println(line);
+                    String[] lines = Arrays.stream(line.split(":",2)).toArray(String[]::new);
+                    if (lines.length != 1 && !lines[0].startsWith(" ")) {
+                        windowsProperties.put(lines[0].trim(), new String[]{lines[1].trim()});
+                        lastKey = lines[0].trim();
+                        continue;
+                    }
+                    windowsProperties.put(lastKey, Arrays
+                            .stream(new String[][]{windowsProperties
+                                    .get(lastKey), new String[]{lines[0]
+                                    .trim()}})
+                            .flatMap(Arrays::stream)
+                            .toArray(String[]::new));
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return windowsProperties;
     }
 }
